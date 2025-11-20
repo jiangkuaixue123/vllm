ff7a63d86cd14c53e191845301e52f238d938775
yangxiaojun0126 <yangxiaojun41@huawei.com>
2025-11-20 20:01:04 +0800

    [AFD]add ffn.sh and online_attn.sh

--- examples/online_serving/afd_step3/ffn.sh
+++ examples/online_serving/afd_step3/ffn.sh
@@ -1,1 +1,11 @@
-vllm fserver "/home/models/DeepSeek-V2-Lite" --tensor_parallel_size=2 --enable_expert_parallel --enforce_eager --afd-config '{"afd_connector":"p2pconnector", "num_afd_stages":"3", "afd_role": "ffn", "afd_extra_config":{"afd_size":"2A2F"}}'
+export ASCEND_RT_VISIBLE_DEVICES=14,15
+export HCCL_BUFFSIZE=2048
+export VLLM_LOGGING_LEVEL=DEBUG
+python -m vllm.entrypoints.afd_ffn_server /home/j00586476/DeepSeek-V2-Lite \
+        --tensor-parallel-size 2 \
+        --enable_expert_parallel \
+        --max_num_batched_tokens 4 \
+        --max_num_seqs 4 \
+        --compilation-config '{"cudagraph_mode": "FULL_DECODE_ONLY","cudagraph_capture_sizes":[4]}'  \
+        --max-model-len 4096 \
+        --afd-config '{"afd_connector":"m2nconnector", "num_afd_stages":"1", "afd_role": "ffn", "afd_extra_config":{"afd_size":"2A2F"}, "compute_gate_on_attention": "True"}'
--- examples/online_serving/afd_step3/online_attn.sh
+++ examples/online_serving/afd_step3/online_attn.sh
@@ -1,1 +1,12 @@
-vllm serve "/home/models/DeepSeek-V2-Lite"  --tensor_parallel_size=2 --enable_expert_parallel --enforce_eager  --afd-config '{"afd_connector":"p2pconnector", "afd_role": "attention", "num_afd_stages":"3","afd_extra_config":{"afd_size":"2A2F"}}'
+# --enforce-eager \
+export ASCEND_RT_VISIBLE_DEVICES=12,13
+export HCCL_BUFFSIZE=2048
+export VLLM_LOGGING_LEVEL=DEBUG
+vllm serve /home/j00586476/DeepSeek-V2-Lite \
+    --tensor-parallel-size 2 \
+    --enable_expert_parallel \
+    --max_num_batched_tokens 4 \
+    --max_num_seqs 4 \
+    --compilation-config '{"cudagraph_mode": "FULL_DECODE_ONLY","cudagraph_capture_sizes":[4]}'  \
+    --max-model-len 4096 \
+    --afd-config '{"afd_connector":"m2nconnector", "afd_role": "attention", "num_afd_stages":"1","afd_extra_config":{"afd_size":"2A2F"}, "compute_gate_on_attention": "True"}'
