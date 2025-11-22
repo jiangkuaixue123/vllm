17d02811a04bb11fd06f34a506b16a4684fb3949
yangxiaojun0126 <yangxiaojun41@huawei.com>
2025-11-22 15:26:47 +0800

    [BugFix]support NPU run p2p

--- examples/online_serving/afd_step3/ffn.sh
+++ examples/online_serving/afd_step3/ffn.sh
@@ -1,11 +1,15 @@
-export ASCEND_RT_VISIBLE_DEVICES=14,15
+# --enforce-eager \
+# --compilation-config '{"cudagraph_mode": "FULL_DECODE_ONLY","cudagraph_capture_sizes":[4]}'  \
+#python -m debugpy --listen 56307 --wait-for-client -m vllm.entrypoints.afd_ffn_server /home/l00851163/afd/DSV2LiteWeight\
+#python -m vllm.entrypoints.afd_ffn_server /home/l00851163/afd/DSV2LiteWeight\
+export ASCEND_RT_VISIBLE_DEVICES=2,3
 export HCCL_BUFFSIZE=2048
 export VLLM_LOGGING_LEVEL=DEBUG
-python -m vllm.entrypoints.afd_ffn_server /home/j00586476/DeepSeek-V2-Lite \
+python -m vllm.entrypoints.afd_ffn_server /home/l00851163/afd/DSV2LiteWeight\
         --tensor-parallel-size 2 \
         --enable_expert_parallel \
         --max_num_batched_tokens 4 \
         --max_num_seqs 4 \
-        --compilation-config '{"cudagraph_mode": "FULL_DECODE_ONLY","cudagraph_capture_sizes":[4]}'  \
+        --enforce-eager \
         --max-model-len 4096 \
-        --afd-config '{"afd_connector":"m2nconnector", "num_afd_stages":"1", "afd_role": "ffn", "afd_extra_config":{"afd_size":"2A2F"}, "compute_gate_on_attention": "True"}'
+        --afd-config '{"afd_connector":"p2pconnector", "num_afd_stages":"1", "afd_role": "ffn", "afd_extra_config":{"afd_size":"2A2F"}, "compute_gate_on_attention": "True"}'
--- examples/online_serving/afd_step3/online_attn.sh
+++ examples/online_serving/afd_step3/online_attn.sh
@@ -1,12 +1,15 @@
 # --enforce-eager \
-export ASCEND_RT_VISIBLE_DEVICES=12,13
+# --compilation-config '{"cudagraph_mode": "FULL_DECODE_ONLY","cudagraph_capture_sizes":[4]}'  \
+# python -m debugpy --listen 56306 --wait-for-client $(which vllm) serve /home/l00851163/afd/DSV2LiteWeight \
+export ASCEND_RT_VISIBLE_DEVICES=0,1
 export HCCL_BUFFSIZE=2048
 export VLLM_LOGGING_LEVEL=DEBUG
-vllm serve /home/j00586476/DeepSeek-V2-Lite \
+vllm serve /home/l00851163/afd/DSV2LiteWeight \
     --tensor-parallel-size 2 \
     --enable_expert_parallel \
     --max_num_batched_tokens 4 \
     --max_num_seqs 4 \
-    --compilation-config '{"cudagraph_mode": "FULL_DECODE_ONLY","cudagraph_capture_sizes":[4]}'  \
+    --enforce-eager \
+    --port 8006 \
     --max-model-len 4096 \
-    --afd-config '{"afd_connector":"m2nconnector", "afd_role": "attention", "num_afd_stages":"1","afd_extra_config":{"afd_size":"2A2F"}, "compute_gate_on_attention": "True"}'
+    --afd-config '{"afd_connector":"p2pconnector", "afd_role": "attention", "num_afd_stages":"1","afd_extra_config":{"afd_size":"2A2F"}, "compute_gate_on_attention": "True"}'
