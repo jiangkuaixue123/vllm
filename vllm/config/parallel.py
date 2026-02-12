0fb8391f955a716743e096f06eb97a08f04de2ae
t00791201 <tangtiangu@huawei.com>
2026-02-12 15:01:32 +0800

    remove redundant code

--- vllm/config/parallel.py
+++ vllm/config/parallel.py
@@ -169,9 +169,6 @@ class ParallelConfig:
     prefills. If the number of tokens in the request is greater than this
     threshold, microbatching will be used. Otherwise, the request will be
     processed in a single batch."""
-    disable_nccl_for_dp_synchronization: bool = False
-    """Forces the dp synchronization logic in vllm/v1/worker/dp_utils.py 
-    to use Gloo instead of NCCL for its all reduce"""
 
     disable_nccl_for_dp_synchronization: bool = False
     """Forces the dp synchronization logic in vllm/v1/worker/dp_utils.py 
