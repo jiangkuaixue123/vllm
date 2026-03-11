166df528fb7177b348f47dc0240c997b6e9e1d3c
yangxiaojun0126 <yangxiaojun41@huawei.com>
2026-03-11 04:03:57 -0400

    overlong line

--- vllm/config/afd.py
+++ vllm/config/afd.py
@@ -48,8 +48,8 @@ class AFDConfig:
     compute_gate_on_attention: bool = False
     """Whether to compute the gate on the attention side."""
 
-    multistream_info: dict[str, Any] = field(default_factory=
-                                             lambda: {"enable": "False","core_num": "8"})
+    multistream_info: dict[str, Any] = 
+        field(default_factory= lambda: {"enable": "False","core_num": "8"})
     """MultiStream configuration:
         1. enable flag;
         2. when enabled, set core_num of communication stream."""
